d708c20112e8e9362bdad54b3664ac9aed00c8fe
Alexander Oguejiofor <oguejioforalexander@gmail.com>
2019-09-06 10:14:53 +0100

    card component

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -17,3 +17,53 @@
 // </div>
 //
 // Create a card for each of the articles and add the card to the DOM.
+
+axios.get('https://lambda-times-backend.herokuapp.com/articles')
+.then(response => { // response is what the promise resolved to (what it looks is up to axios)
+    console.log(response.data);
+    let articleArr = []
+  for (let key in response.data){
+      console.log(response.data[key]);
+      for (let key2 in response.data[key]){
+          console.log(response.data[key][key2]);
+          response.data[key][key2].forEach(element => {
+              cardsContainer.appendChild(cardCreator(element));
+          });
+      }
+      
+  }
+})
+.catch(error => { // error is what the promise rejected to (what it looks like depends on axios)
+  document.body.innerText = error.message;
+});
+
+
+const cardsContainer = document.querySelector('.cards-container');
+
+function cardCreator (arr){
+    const cardDiv = document.createElement('div');
+    const headlineDiv = document.createElement('div');
+    const authorDiv = document.createElement('div');
+    const imgContainer = document.createElement('div');
+    const image = document.createElement('img');
+    const authorSpan = document.createElement('span');
+
+    cardDiv.classList.add('card');
+    headlineDiv.classList.add('headline');
+    authorDiv.classList.add('author');
+    imgContainer.classList.add('img-container');
+
+    headlineDiv.textContent = arr.headline;
+    image.setAttribute('src', arr.authorPhoto);
+    authorSpan.textContent = "By " + arr.authorName;
+
+    imgContainer.appendChild(image);
+    authorDiv.appendChild(imgContainer);
+    authorDiv.appendChild(authorSpan);
+    cardDiv.appendChild(headlineDiv);
+    cardDiv.appendChild(authorDiv);
+
+    return cardDiv;
+}
+
+
--- components/Tabs/index.js
+++ components/Tabs/index.js
@@ -7,3 +7,30 @@
 //
 //  The tab component should look like this:
 //    <div class="tab">topic here</div>
+
+// axios.get('https://lambda-times-backend.herokuapp.com/topics')
+//     .then(response => { // response is what the promise resolved to (what it looks is up to axios)
+//         // debugger
+//         console.log(response.data);
+//         console.log(tabDivCreator(response.data));
+//     })
+//     .catch(error => { // error is what the promise rejected to (what it looks like depends on axios)
+//       document.body.innerText = error.message;
+//     });
+
+//     const topics = document.querySelector('.topics');
+
+//     function tabDivCreator (titleObject) {
+//         const topicObject = titleObject;
+//         console.log(topicObject);
+//         const topicArray = topicObject.topics;
+//         console.log(topicArray);
+
+//         topicArray.forEach(topic => {
+//             const tabDiv = document.createElement('div');
+//             tabDiv.classList.add('tab');
+//             tabDiv.textContent = topic;
+//             return tabDiv;
+//         });   
+//         return tabDiv;
+//     }
